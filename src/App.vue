3f9294403ff2670a9fc3c602991bac43ae8e358e
ink16 <rares@jekyo.com>
2021-10-10 19:33:30 +0300

    created hello world vuejs app

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div><h1>This is a basic Vue app deployed on {{ value }}</h1></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      value: "Jekyo"
+    };
+  }
+};
+</script>
